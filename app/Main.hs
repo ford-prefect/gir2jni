a3a53adf2c2b5780a8ffa7e7cc21287afca44dc6
Arun Raghavan <arun@arunraghavan.net>
2016-07-06 17:12:54 +0530

    Basic code to read a GIR file
    Using haskell-gi since it seems to do exactly what we need.

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,49 @@ module Main where
 
 import Lib
 
+import qualified Data.Text as T (Text, pack)
+import Options.Applicative
+
+import Data.GI.CodeGen.API (loadGIRInfo, GIRInfo)
+
+data Opts = Opts { optGIRName    :: T.Text
+                 , optGIRVersion :: Maybe T.Text
+                 , optOutputDir  :: String
+                 , optVerbose    :: Bool   }
+
+parseOptions :: Parser Opts
+parseOptions = Opts
+           <$> (T.pack <$> strOption
+               ( long "gir-name"
+              <> short 'n'
+              <> metavar "GIR"
+              <> help "GIR package name to generate bindigs for" ) )
+           <*> option (str >>= return . Just . T.pack)
+               ( long "gir-version"
+              <> short 'V'
+              <> metavar "VER"
+              <> value Nothing
+              <> help "GIR package version to use" )
+           <*> strOption
+               ( long "output"
+              <> short 'o'
+              <> value "-"
+              <> metavar "DIR"
+              <> help "Output path for generated Java and C code" )
+           <*> switch
+               ( long "verbose"
+              <> short 'v'
+              <> help "Verbose output")
+
+
+run :: Opts -> IO ()
+run opts = do
+  (gir, girDeps) <- loadGIRInfo (optVerbose opts) (optGIRName opts) (optGIRVersion opts) [] []
+  return ()
+
 main :: IO ()
-main = someFunc
+main = execParser opts >>= run
+  where
+    opts = info (helper <*> parseOptions)
+      ( fullDesc
+     <> header "gir2jni - Java+JNI generation from GObject Introspection data" )
