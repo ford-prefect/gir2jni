3ae6405ec4e38cc7380bb79fad6f973342e4632f
Arun Raghavan <arun@arunraghavan.net>
2016-07-11 19:12:25 +0530

    Implement loading and parsing overrides files

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,16 @@ module Main where
 
 import Lib
 
-import qualified Data.Text as T (Text, pack)
+import qualified Data.Text as T (Text, lines, pack, unpack)
+import qualified Data.Text.IO as TIO (readFile)
 import Options.Applicative
 
-import Data.GI.CodeGen.API (loadGIRInfo, GIRInfo)
+import Data.GI.CodeGen.API (loadGIRInfo)
+import Data.GI.CodeGen.Overrides (Overrides, filterAPIsAndDeps, girFixups, parseOverridesFile)
 
 data Opts = Opts { optGIRName    :: T.Text
                  , optGIRVersion :: Maybe T.Text
+                 , optOverrides  :: Maybe FilePath -- This will need to become a list
                  , optOutputDir  :: String
                  , optVerbose    :: Bool   }
 
@@ -25,9 +28,15 @@ parseOptions = Opts
               <> metavar "VER"
               <> value Nothing
               <> help "GIR package version to use" )
+           <*> option (str >>= return . Just)
+               ( long "gir-override"
+              <> short 'o'
+              <> metavar "OV"
+              <> value Nothing
+              <> help "GIR override file" )
            <*> strOption
                ( long "output"
-              <> short 'o'
+              <> short 'O'
               <> value "-"
               <> metavar "DIR"
               <> help "Output path for generated Java and C code" )
@@ -39,7 +48,16 @@ parseOptions = Opts
 
 run :: Opts -> IO ()
 run opts = do
-  (gir, girDeps) <- loadGIRInfo (optVerbose opts) (optGIRName opts) (optGIRVersion opts) [] []
+  ovsFile   <- case optOverrides opts of
+                 Nothing -> return []
+                 Just f  -> T.lines <$> (TIO.readFile f)
+  -- Parsing an empty file seems to work, but if not, we should use mempty
+  ovsParsed <- parseOverridesFile ovsFile
+  let ovs = case ovsParsed of
+              Left err -> error ("Error parsing overrides file: " `mappend` T.unpack err)
+              Right o  -> o
+  (gir, girDeps) <- loadGIRInfo (optVerbose opts) (optGIRName opts) (optGIRVersion opts) [] (girFixups ovs)
+  let (apis, deps) = filterAPIsAndDeps ovs gir girDeps
   return ()
 
 main :: IO ()
