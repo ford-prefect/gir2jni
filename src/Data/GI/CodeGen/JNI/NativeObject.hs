0f7ffe802a010a011e70bbf0fab9a555a1701247
Arun Raghavan <arun@arunraghavan.net>
2016-09-09 12:01:15 +0530

    object: Minor NativeObject refactor
    Move top-level code that isn't exported into a where clause.

--- src/Data/GI/CodeGen/JNI/NativeObject.hs
+++ src/Data/GI/CodeGen/JNI/NativeObject.hs
@@ -23,31 +23,6 @@ nativeObjectFieldIdent = "pointer"
 nativeObjectSetterIdent :: String
 nativeObjectSetterIdent = "_setPointer"
 
-genNativeObjectJava :: Info -> JSyn.CompilationUnit
-genNativeObjectJava Info{..} =
-  let
-    pkg       = infoPkgPrefix
-    name      = nativeObjectIdent
-    modifiers = [JSyn.Public, JSyn.Abstract]
-    ptrIdent  = JSyn.VarId . JSyn.Ident $ nativeObjectFieldIdent
-    ptrInit   = Just. JSyn.InitExp . JSyn.Lit . JSyn.Int $ 0
-    field     = JSyn.FieldDecl [] (JSyn.PrimType JSyn.LongT) [JSyn.VarDecl ptrIdent ptrInit]
-    decls     = JSyn.MemberDecl <$> [field, nativeObjectSetterMethod]
-  in
-    genJavaClass pkg name modifiers Nothing [] decls
-  where
-    nativeObjectSetterMethod =
-      let
-        mods   = [JSyn.Protected, JSyn.Final]
-        ident  = JSyn.Ident "object"
-        param  = JSyn.FormalParam [] (JSyn.PrimType JSyn.LongT) False (JSyn.VarId ident)
-        from   = JSyn.ExpName . JSyn.Name $ [ident]
-        to     = JSyn.FieldLhs . JSyn.PrimaryFieldAccess JSyn.This . JSyn.Ident $ nativeObjectFieldIdent
-        assign = JSyn.Assign to JSyn.EqualA from
-        body   = JSyn.MethodBody . Just . JSyn.Block $ [JSyn.BlockStmt . JSyn.ExpStmt $ assign]
-      in
-        JSyn.MethodDecl mods [] Nothing (JSyn.Ident nativeObjectSetterIdent) [param] [] body
-
 -- | NativeObject is a fake parent class of GObject that keeps a strong ref to
 --   the object within the Java object, and drops the ref when the Java object
 --   goes out of scope
@@ -58,3 +33,28 @@ genNativeObject info@Info{..} =
     jCode = genNativeObjectJava info
   in
     ((cls, jCode), [])
+  where
+    genNativeObjectJava :: Info -> JSyn.CompilationUnit
+    genNativeObjectJava Info{..} =
+      let
+        pkg       = infoPkgPrefix
+        name      = nativeObjectIdent
+        modifiers = [JSyn.Public, JSyn.Abstract]
+        ptrIdent  = JSyn.VarId . JSyn.Ident $ nativeObjectFieldIdent
+        ptrInit   = Just. JSyn.InitExp . JSyn.Lit . JSyn.Int $ 0
+        field     = JSyn.FieldDecl [] (JSyn.PrimType JSyn.LongT) [JSyn.VarDecl ptrIdent ptrInit]
+        decls     = JSyn.MemberDecl <$> [field, nativeObjectSetterMethod]
+      in
+        genJavaClass pkg name modifiers Nothing [] decls
+      where
+        nativeObjectSetterMethod =
+          let
+            mods   = [JSyn.Protected, JSyn.Final]
+            ident  = JSyn.Ident "object"
+            param  = JSyn.FormalParam [] (JSyn.PrimType JSyn.LongT) False (JSyn.VarId ident)
+            from   = JSyn.ExpName . JSyn.Name $ [ident]
+            to     = JSyn.FieldLhs . JSyn.PrimaryFieldAccess JSyn.This . JSyn.Ident $ nativeObjectFieldIdent
+            assign = JSyn.Assign to JSyn.EqualA from
+            body   = JSyn.MethodBody . Just . JSyn.Block $ [JSyn.BlockStmt . JSyn.ExpStmt $ assign]
+          in
+            JSyn.MethodDecl mods [] Nothing (JSyn.Ident nativeObjectSetterIdent) [param] [] body
